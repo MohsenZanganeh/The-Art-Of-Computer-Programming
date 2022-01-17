c9591222d769232978f977c5ab33b3db5c394a5b
mohsenZanganeh <mohsen.1398.pg@gmail.com>
2022-01-17 10:07:20 +0330

    i corrected the exercise of page 16

--- page-26(common-divisor).py
+++ page-26(common-divisor).py
@@ -1,25 +1,40 @@
 # we want to find common divisor for two number
-
-from tokenize import Number
-
-
 first_number  = int(input('First Number:'))
 second_number = int(input('Second Number:'))
 
-is_not_common = True
+
+# E0: if first_number < second_number
+#     --yes-> 
+#            first_number <-> second_number, go to next step
+#     --No->
+#           go to next Step
+# E1: if first_number/second_number = quotion + reminder/ second_number  
+#     --yes->
+# E2:        if reminder != 0 
+#            --yes-> 
+# E3:               first_number <- reminder , first_number <-> second_number, go to next step
+#             --No-> 
+#                   Terminate , go to next step
+
+if first_number < second_number:
+    tmp = first_number  
+    first_number = second_number
+    second_number = tmp
+
 quotion = first_number//second_number
 reminder = first_number % second_number
+
 while first_number/second_number == quotion + reminder/ second_number:
-    if first_number < second_number:
+
+    if reminder != 0:
+        first_number = reminder
+
         tmp = first_number  
         first_number = second_number
         second_number = tmp
-
-    reminder = first_number % second_number
-    if reminder != 0:
-        first_number = reminder
-        quotion = reminder
+        quotion = first_number//second_number
+        reminder = first_number % second_number
     else:
-        is_not_common = False
+        break
 
-print('common divisor:',quotion)
+print('common divisor:',second_number)
